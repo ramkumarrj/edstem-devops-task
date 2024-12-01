b5388582991fdca88d7e153403ec1f14ec7a6e90
Ramkumar <ramkumarrj1002@gmail.com>
2024-12-01 18:39:12 +0530

    update node file

--- myapp/tests/app.test.js
+++ myapp/tests/app.test.js
@@ -6,6 +6,6 @@ describe('GET /', () => {
     it('should return welcome message', async () => {
         const response = await request(app).get('/');
         expect(response.statusCode).toBe(200);
-        expect(response.text).toContain('Welcome to My Node.js Application!');
+        expect(response.text).toContain('Welcome to testing');
     });
 });
